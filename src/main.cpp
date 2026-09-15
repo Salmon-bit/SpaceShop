d10918088f5b70b5685b81ede2e7b8c1541e3da0
Salmon228 <salmon228@inbox.ru>
2026-09-15 22:11:24 +0300

    Basic Configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace sf;
+using namespace std;
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+	RenderWindow window(VideoMode({200, 200}), "Space Shop");
+	CircleShape shape(100.f);
+	shape.setFillColor(Color::Green);
 
-	while ( window.isOpen() )
+	while (window.isOpen())
 	{
-		while ( const std::optional event = window.pollEvent() )
+		while (const optional event = window.pollEvent())
 		{
-			if ( event->is<sf::Event::Closed>() )
+			if (event -> is < Event::Closed>())
 				window.close();
+				cout << "Closing Window..." << endl;
 		}
 
 		window.clear();
-		window.draw( shape );
+		window.draw(shape);
 		window.display();
 	}
+
+	return 0;
 }
